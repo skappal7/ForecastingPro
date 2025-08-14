bf6d9462d0701f15439ab771ff0f26eafa48aa3d
Sunil Kappal <skappal7@users.noreply.github.com>
2025-08-14 16:14:28 +0530

    Update forecastpro.py

--- forecastpro.py
+++ forecastpro.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import timedelta
-from prophet import Prophet
+from neuralprophet import NeuralProphet
 import pmdarima as pm
 from sklearn.ensemble import IsolationForest
 import plotly.graph_objects as go
